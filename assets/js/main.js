9b0e9099ba5c4b901d4c4f01aff6d2ee6d45f8a8
Giulia Ferreira <giuliabferreira@hotmail.com>
2023-04-21 21:24:08 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,73 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu("nav-toggle", "nav-menu");
+
+
+/* MOSTRAR DROPDOWN MENU */
+
+const dropdownItems = document.querySelectorAll('.dropdown__item')
+
+
+// 1. SELECIONAR CADA ITEM DO DROPDOWN
+
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector('.dropdown__button')
+
+  // 2. SELECIONAR CADA CLICK DO BUTTON
+  
+  dropdownButton.addEventListener('click', () => {
+    // 7. SELECIONAR A ATUAL CLASSE SHOW-DROPDOWN
+    const showDropdown = document.querySelector('.show-dropdown')
+
+    // 5. CHAMAR A FUNÇÃO TOGGLEITEM
+    toggleItem(item)
+
+    // 8. REMOVER A CLASSE SHOW-DROPDOWN PARA OS OUTROS ITENS
+    if(showDropdown && showDropdown!= item) {
+      toggleItem(showDropdown)
+    }
+  })
+})
+
+
+// 3. CRIAR UMA FUNÇÃO PARA O DROPDOWN ABAIXAR
+
+const toggleItem = (item) => {
+  // 3.1. SELECIONAR CADA CONTEÚDO DO DROPDOWN
+  const dropdownContainer = item.querySelector('.dropdown__container')
+
+  // 6. SE O MESMO ITEM CONTER A CLASSE SHOW-DROPDOWN, REMOVER
+  if(item.classList.contains('show-dropdown')) {
+    dropdownContainer.removeAttribute('style')
+    item.classList.remove('show-dropdown')
+
+  } else {
+    // 4. ADICIONAR O MÁXIMO DE TAMANHO NO CONTEÚDO DO DROPDOWN E ADICIONAR O SHOW-DROPDOWN
+  dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+  item.classList.add('show-dropdown')
+  }
+}
+
+/* DELETAR OS ESTILOS DO DROPDOWN */
+
+const mediaQuery = matchMedia('(min-width: 1024px)'),
+  dropdownContainer = document.querySelectorAll('.dropdown__container')
+
+
+// FUNÇÃO DE REMOVER O ESTILO DO DROPDOWN NO MOBILE QUANDO O BROWSER SE REALINHAR
+const removeStyle = () => {
+  // VALIDAR SE A MEDIA QUERY ALCANÇAR 1118PX
+  if(mediaQuery.matches) {
+    // REMOVER O TAMANHO E ESTILO DO CONTAINER DROPDOWN 
+    dropdownContainer.forEach((e) => {
+      e.removeAttribute('style')
+    })
+
+    // REMOVER A CLASSE SHOW-DROPDOWN PARA ITENS DROPDOWN
+    dropdownItems.forEach((e) => {
+      e.classList.remove('show-dropdown')
+    })
+  }
+}
+
+addEventListener('resize', removeStyle)
